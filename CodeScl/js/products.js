a10cd8130704b2c9270c234be99c9ba53fa8820d
Ritesh <riteshtripathi.0412@gmail.com>
2017-01-06 01:10:42 +0530

    Last Commit

--- CodeScl/js/products.js
+++ CodeScl/js/products.js
@@ -1,38 +1,36 @@
 /**
  * Created by Ritesh on 10/23/2016.
  */
-(function(){
-    var app = angular.module('store-products',[]);
+(function () {
+    var app = angular.module('store-products', []);
 
-
-    app.controller('ReviewControler',function(){
-        this.review={};
-        this.addReview = function(product){
+    app.controller('ReviewControler', function () {
+        this.review = {};
+        this.addReview = function (product) {
             product.reviews.push(this.review);
-            this.review={};
-
-        }
+            this.review = {};
+       }
     });
 
 
-    app.directive('productTitle',function(){
-        return{
-            restrict:'E',
-            templateUrl:'product-title.html'
+    app.directive('productTitle', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-title.html'
         }
     });
 
-    app.directive('productPanel',function(){
-        return{
+    app.directive('productPanel', function () {
+        return {
             restrict: 'E',
             templateUrl: 'product-panel.html',
-            controller:function(){
+            controller: function () {
                 this.tab = 1;
 
-                this.setTab = function(tabVal){
+                this.setTab = function (tabVal) {
                     this.tab = tabVal;
                 }
-                this.isSelected = function(checkTab){
+                this.isSelected = function (checkTab) {
                     return this.tab === checkTab;
                 }
             },
